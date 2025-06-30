78dd04d416853abf5c96161ddd22d3d7d887c29f
Kerem Ajp <kerem@example.com>
2025-06-30 19:44:17 +0300

    fix: clean up duplicate imports in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,13 @@ import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {defineConfig} from 'vite';
-import {hydrogen} from '@shopify/hydrogen/vite';
-import {oxygen} from '@shopify/mini-oxygen/vite';
-import {reactRouter} from '@react-router/dev/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [hydrogen(), oxygen(), reactRouter(), tsconfigPaths()],
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
-      external: ['react-router-dom/server'], // 👈 bu satırı ekledik
+      external: ['react-router-dom/server'], // 🚫 hatayı engelleyen satır
     },
   },
   ssr: {
