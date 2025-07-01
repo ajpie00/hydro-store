f852fa1587dfc80b359715a4819fecf062a85123
Kerem Ajp <kerem@example.com>
2025-07-01 21:06:42 +0300

    fix: correct  route for deployment

--- app/routes/$locale.jsx
+++ app/routes/$locale.jsx
@@ -1,5 +1,3 @@
-import {Outlet} from '@remix-run/react';
-
-export default function LocaleLayout() {
-  return <Outlet />;
+export default function LocaleLayout({children}) {
+  return <>{children}</>;
 }
