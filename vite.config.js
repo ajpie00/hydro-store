21225f4a6d8f0104c537b1efc0915836d6ff8d3e
Kerem Ajp <kerem@example.com>
2025-07-14 23:47:31 +0300

    fix: add fsRoutes for Vercel to recognize dynamic routes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,22 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
+import fsRoutes from '@react-router/fs-routes'; // 🔥 bu satırı ekle
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [hydrogen(), oxygen(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    hydrogen({
+      routes: fsRoutes('app/routes'), // 🔥 burada route dizinini tanımlıyoruz
+    }),
+    oxygen(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
-      external: ['react-router-dom/server'], // 🚫 hatayı engelleyen satır
+      external: ['react-router-dom/server'],
     },
   },
   ssr: {
