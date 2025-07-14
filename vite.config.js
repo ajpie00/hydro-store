79ea6863cc933064feab3b3da7f252dbc0f89037
Kerem Ajp <kerem@example.com>
2025-07-14 23:52:06 +0300

    fix: remove fsRoutes plugin; reactRouter handles routing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
-import fsRoutes from '@react-router/fs-routes'; // 🔥 bu satırı ekle
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
